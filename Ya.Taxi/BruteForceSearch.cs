672b3a509bdde54f98ac961f2dc76133e669f1c9
Andrew Gergert <adm-insert@ya.ru>
2018-02-25 17:26:12 +0600

    Major optimization
    * added multithreading
    * changed IsValidRoute Stream to ParallelStream

--- Ya.Taxi/BruteForceSearch.cs
+++ Ya.Taxi/BruteForceSearch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Ya.Taxi
 {
@@ -10,10 +11,7 @@ namespace Ya.Taxi
         /// <summary>
         /// Return a dictionary with succeeded movements (distinct by weights)
         /// </summary>
-        public Dictionary<int, string> SuccededMovementDictionary
-        {
-            get { return _successfulMovements; }
-        }
+        public Dictionary<int, string> SuccededMovementDictionary => _successfulMovements;
 
         /// <summary>
         /// Constructor by default (5x5 field)
@@ -40,7 +38,6 @@ namespace Ya.Taxi
                 Console.WriteLine($"{e.Message}\n");
                 _field = new Field();
             }
-
             DoBruteForce();
         }
 
@@ -49,29 +46,82 @@ namespace Ya.Taxi
         /// </summary>
         private void DoBruteForce()
         {
-            _field.ViewField();
-            Console.WriteLine("Building routes...");
-            _successfulMovements = new Dictionary<int, string>();
-            int bitCapacityNeeded = (_field.Height - 1) + (_field.Width - 1);
-            Int64 rangeFloor =
+            int bitCapacityNeeded;
+            long rangeFloor, rangeCeiling;
+            lock (_field)
+            {
+                _field.ViewField();
+                Console.WriteLine("Building routes...");
+                _successfulMovements = new Dictionary<int, string>();
+                bitCapacityNeeded = (_field.Height - 1) + (_field.Width - 1);
+                rangeFloor =
                     Convert.ToInt64(
                         new StringBuilder().Append('0', (_field.Height + _field.Width - 2) / 2)
-                            .Append('1', (_field.Height + _field.Width - 2) / 2).ToString(), 2),
+                            .Append('1', (_field.Height + _field.Width - 2) / 2).ToString(), 2);
                 rangeCeiling = Convert.ToInt64(
                     new StringBuilder().Append('1', (_field.Height + _field.Width - 2) / 2)
                         .Append('0', (_field.Height + _field.Width - 2) / 2).ToString(), 2);
-            for (Int64 i = rangeFloor; i <= rangeCeiling; i++)
+            }
+
+            Thread[] threads = new Thread[_threadsCount];
+            Parameter[] parameters = new Parameter[_threadsCount];
+            for (int i = 0; i < _threadsCount; i++)
             {
-                string movement = FormatRoute(Convert.ToString(i, 2), bitCapacityNeeded);
-                _field.ResetPosition();
-                int sum;
-                if (IsValidRoute(movement) && !_successfulMovements.ContainsKey(sum = RouteNumbersSum(movement)))
+                threads[i] = new Thread(PerformUniqueTask);
+                parameters[i] = new Parameter(_threadsCount, i, rangeFloor, rangeCeiling, bitCapacityNeeded);
+                threads[i].Start(parameters[i]);
+            }
+            while (threads.Any(t => t.IsAlive))
+                Thread.Sleep(500);
+            Console.WriteLine("All routes built.");
+        }
+
+        /// <summary>
+        /// Inner class to use in DoInThread
+        /// </summary>
+        class Parameter
+        {
+            public Parameter(int threadsCount, int shift, long rangeFloor, long rangeCeiling, int bitCapacityNeeded)
+            {
+                ThreadsCount = threadsCount;
+                Shift = shift;
+                RangeFloor = rangeFloor;
+                RangeCeiling = rangeCeiling;
+                BitCapacityNeeded = bitCapacityNeeded;
+            }
+
+            public int ThreadsCount { get; }
+            public int Shift { get; }
+            public int BitCapacityNeeded { get; }
+            public long RangeCeiling { get; }
+            public long RangeFloor { get; }
+        }
+
+        /// <summary>
+        /// Perform one unique task within field
+        /// </summary>
+        /// <param name="o">Parameter object</param>
+        private void PerformUniqueTask(object o)
+        {
+            Parameter p = (Parameter)o;
+
+            for (long i = p.Shift + p.RangeFloor; i <= p.RangeCeiling; i += p.ThreadsCount)
+            {
+                string movement = FormatRoute(Convert.ToString(i, 2), p.BitCapacityNeeded);
+                if (IsValidRoute(movement))
                 {
-                    _successfulMovements.Add(sum, movement);
-                    Console.WriteLine($"Route found [{movement}]" + " with weight [{0}]", sum.ToString().PadLeft(4));
+                    lock (_successfulMovements)
+                    {
+                        int sum;
+                        if (!_successfulMovements.ContainsKey(sum = RouteNumbersSum(movement)))
+                        {
+                            _successfulMovements.Add(sum, movement);
+                            Console.WriteLine($"Route found [{movement}]" + " with weight [{0}]",
+                                sum.ToString().PadLeft(4));
+                        }
+                    }
                 }
             }
-            Console.WriteLine("All routes built.");
         }
 
         /// <summary>
@@ -82,7 +132,7 @@ namespace Ya.Taxi
         /// <returns></returns>
         private bool IsValidRoute(string route)
         {
-            int enabledBitCount = route.Count(p => p == '1');
+            int enabledBitCount = route.AsParallel().Count(p => p == '1');
             return (enabledBitCount == _field.Height - 1) && (route.Length - enabledBitCount == _field.Width - 1);
         }
 
@@ -93,26 +143,29 @@ namespace Ya.Taxi
         /// <returns></returns>
         private int RouteNumbersSum(string route)
         {
-            _field.ResetPosition();
-            int sum = 0;
-            foreach (char c in route)
+            lock (_field)
             {
-                switch (c)
+                _field.ResetPosition();
+                int sum = 0;
+                foreach (char c in route)
                 {
-                    case '0':
+                    switch (c)
+                    {
+                        case '0':
 
-                        sum += _field.FieldArray[_field.XCoord, _field.YCoord];
-                        _field.Move(_field.XCoord + 0, _field.YCoord);
-                        break;
-                    case '1':
-                        sum += _field.FieldArray[_field.XCoord, _field.YCoord];
-                        _field.Move(_field.XCoord, _field.YCoord + 1);
-                        break;
+                            sum += _field.FieldArray[_field.XCoord, _field.YCoord];
+                            _field.Move(_field.XCoord + 1, _field.YCoord);
+                            break;
+                        case '1':
+                            sum += _field.FieldArray[_field.XCoord, _field.YCoord];
+                            _field.Move(_field.XCoord, _field.YCoord + 1);
+                            break;
+                    }
+
+                    if (_field.XCoord == _field.Width - 1 && _field.YCoord == _field.Height - 1) return sum;
                 }
-
-                if (_field.XCoord == _field.Width - 1 && _field.YCoord == _field.Height - 1) return sum;
+                return sum;
             }
-            return sum;
         }
 
         /// <summary>
@@ -128,5 +181,6 @@ namespace Ya.Taxi
 
         private Field _field;
         private Dictionary<int, string> _successfulMovements;
+        private readonly int _threadsCount = Environment.ProcessorCount;
     }
 }
